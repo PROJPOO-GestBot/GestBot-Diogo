a3518efc2e695097b4a7db2f6b0a8d6f7edd2196
Diogo da Silva Fernandes <diogo.da-silva-fernandes@cpnv.ch>
2022-11-25 10:07:14 +0100

    Add base python bot

---
+++ bot.py
@@ -0,0 +1,13 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f"The bot is now ready : {client.user}")
+    
+client.run(os.getenv('BOT_KEY'))
