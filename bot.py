8cc0b8ed348c17253055828e8bb69729ba81260d
Diogo da Silva Fernandes <diogo.da-silva-fernandes@cpnv.ch>
2022-11-25 12:12:52 +0100

    Add lavalink server node

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import wavelink
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,8 +11,19 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')    
 
+async def lavalink_nodes_connect():
+    """Connect to our Lavalink nodes."""
+    
+    await wavelink.NodePool.create_node(
+        bot=bot,
+        host='127.0.0.1',
+        port=2333,
+        password='Pa$$w0rd'
+    ) # create the node
+
 @bot.event
 async def on_ready():
+    await lavalink_nodes_connect()
     print(f"The bot is now ready : {bot.user}")
 
 bot.run(os.getenv('BOT_KEY'))
